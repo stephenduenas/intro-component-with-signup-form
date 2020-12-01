fd261843dc89214849891760670646d530a392c4
Stephen Duenas <semduenas@gmail.com>
2020-12-01 18:28:58 +0800

    Pub files + Vue, sass setup

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <SignUpForm></SignUpForm>
+    <Footer></Footer>
+  </div>
+</template>
+
+<script>
+import SignUpForm from './components/SignUpForm'
+import Footer from './components/Footer'
+
+export default {
+  name: 'App',
+  components: {
+    SignUpForm,
+    Footer
+  }
+}
+</script>
+
+<style lang="scss">
+  @import "./assets/sass/main.scss";
+</style>
---
+++ src/components/SignUpForm.vue
@@ -0,0 +1,31 @@
+<template>
+<div>
+    <h1>Sign Up Form</h1>
+
+    Learn to code by watching others
+
+    See how experienced developers solve problems in real-time. Watching scripted tutorials is great, 
+    but understanding how developers think is invaluable. 
+
+    Try it free 7 days then $20/mo. thereafter
+
+    First Name 
+    Last Name 
+    Email Address
+    Password
+
+    Claim your free trial 
+
+    By clicking the button, you are agreeing to our Terms and Services
+</div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
